4e4126c33bed34b6731619d9d111c624a374c1a9
AndreiVed <catblues87@gmail.com>
2025-07-22 15:08:50 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 from typing import Callable
 
 
-def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+def cache(func):
+  cached_result = {}
+  def wripper(*args, **kwargs):
+    a, b, c = args
+    if (a, b, c) not in cached_result:
+      result = func(a, b, c)
+      cached_result[a, b, c] = result
+      print("Calculating new result")
+    else:
+      print ("Getting from cache")
+  return wripper
